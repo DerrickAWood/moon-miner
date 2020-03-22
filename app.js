27004d6fb7f7d31438eb627feb6a23a8741fb21d
Derrick A Wood <dwood_1994@yahoo.com>
2020-03-21 20:33:20 -0600

    four choices

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ let pickAxe = 0
 let pickAxeElem = document.getElementById("axe")
 let cost = 5
 let roverCost = 20
-let roverElem = document.getElementById("buy-axe")
+let roverCostElem = document.getElementById("roverCost")
+let roverElem = document.getElementById("roverElem")
 let rover = 0
+let hammerCost = 10
+let JackHammer = 0
+let hammerElem = document.getElementById("hammerElem")
+let ExcavatorCost = 15
+let Excavator = 0
+let excavatorElem = document.getElementById("excavatorElem")
+
 
 let choices = [
   {
@@ -53,13 +61,47 @@ function addTool(){
   }
 }
 
+
+function addJackhammer(){
+  if(clickCount >= hammerCost){
+    clickCount-= hammerCost
+    moonRockElem.innerText = clickCount.toString()
+    JackHammer++
+    hammerElem.innerText = JackHammer.toString()
+    hammerCost++
+    document.getElementById("hammerCostElem").innerText = hammerCost.toString()
+    clickCount*= 3
+    // addRocks+= clickCount
+    // clickCount = addRocks
+  }
+}
+
+
+function addExcavator(){
+  if(clickCount >= ExcavatorCost){
+    clickCount-= ExcavatorCost
+    moonRockElem.innerText = clickCount.toString()
+    Excavator++
+    excavatorElem.innerText = Excavator.toString()
+    ExcavatorCost++
+    document.getElementById("excavatorCostElem").innerText = ExcavatorCost.toString()
+    clickCount*= 4
+    // addRocks+= clickCount
+    // clickCount = addRocks
+  }
+}
+
+
+
+
 function addRover(){
   if(clickCount >= roverCost){
     clickCount-= roverCost
     moonRockElem.innerText = clickCount.toString()
     roverCost++
+    roverCostElem.innerText = roverCost.toString()
+    rover++
     roverElem.innerText = rover.toString()
-    // not working //
     roverClicked()
   }
 }
